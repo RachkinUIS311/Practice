9e895af1e2dda90d05ff288cf3dbb9ac3e10ff4a
Nuclear-Fox <grid30@mail.ru>
2022-07-20 15:40:31 +0300

    Добавлены методы getDoc и getDocJSON

--- vu36/src/main/java/com/mycompany/server/jaxrs/dao/DBMethods.java
+++ vu36/src/main/java/com/mycompany/server/jaxrs/dao/DBMethods.java
@@ -51,4 +51,40 @@ public class DBMethods {
         conn.close();
         return list;
     }
+    
+    public static VU36Model getDoc(int id) throws Exception, SQLException
+    {
+        
+        Class.forName("org.postgresql.Driver");
+            
+        String url = "jdbc:postgresql://localhost:5432/db";
+        String user = "Admin";
+        String pass = "123456";
+        Connection conn = DriverManager.getConnection(url, user, pass);
+        Statement stmt = conn.createStatement();
+        StringBuilder query = new StringBuilder();
+        
+        query.append(" select vu_36.id, vu_36.vagnum, nsi.nsi_fname, vu_36.tsn, vu_36.tsk, vu_36.nazv ");
+        query.append(" from vu_36 ");
+        query.append(" inner join nsi on remtype = nsi_kod and nsi_spr = 86 and vu_36.tsk between nsi.nsi_datan and nsi.nsi_datak ");
+        query.append(" inner join nsi as t1 on vagtype = t1.nsi_kod and t1.nsi_spr= 34 and vu_36.tsk between t1.nsi_datan and t1.nsi_datak ");
+        query.append(" where vu_36.id = " + id);
+        ResultSet rs = stmt.executeQuery(query.toString());
+        
+        VU36Model doc = new VU36Model();
+        if (rs.next()) {
+            doc.setId(rs.getInt("id"));
+            doc.setVagnum(rs.getString("vagnum"));
+            doc.setRemtype(rs.getString("nsi_fname"));
+            doc.setTsn(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(rs.getTimestamp("tsn")));
+            doc.setTsk(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(rs.getTimestamp("tsk")));
+            doc.setSnazv(rs.getString("nazv"));
+        }
+        
+        rs.close();
+        stmt.close();
+        conn.close();
+        return doc;
+    }
+    
 }
--- vu36/src/main/java/com/mycompany/server/jaxrs/service/ServerService.java
+++ vu36/src/main/java/com/mycompany/server/jaxrs/service/ServerService.java
@@ -10,6 +10,7 @@ import com.mycompany.server.jaxrs.dao.*;
 import java.sql.SQLException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,11 +20,22 @@ public class ServerService {
     @GET
     @Path("/getlist")
     @Produces(MediaType.APPLICATION_JSON)
-    public VU36List getJSON () throws SQLException, Exception
+    public VU36List getListJSON () throws SQLException, Exception
     {
         DBMethods dbmethods = new DBMethods();
         VU36List list = new VU36List();
         list = dbmethods.getList();
         return list;
+    }
+    
+    @GET
+    @Path("/getdoc")
+    @Produces(MediaType.APPLICATION_JSON)
+    public VU36Model getDocJSON (@QueryParam("id") int id) throws SQLException, Exception
+    {
+        DBMethods dbmethods = new DBMethods();
+        VU36Model doc = new VU36Model();
+        doc = dbmethods.getDoc(id);
+        return doc;
     }
 }
